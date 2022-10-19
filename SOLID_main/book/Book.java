6ad98f082cce9eeccf3dcf37bf5db32c06ca470f
thephruek <69186622+thephruek@users.noreply.github.com>
2022-10-19 20:06:47 +0700

    Add files via upload

---
+++ SOLID_main/book/Book.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ListIterator;
+
+public class Book {
+    private String title;
+    private List<String> pages;
+    private ListIterator<String> iterator;
+
+    public Book(String title, List<String> pages) {
+        this.title = title;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ListIterator<String> getPagesIterator() {
+        return pages.listIterator();
+    }
+}
